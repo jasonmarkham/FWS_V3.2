4e56d6552ab19da623a07c8ae29cb87bfea8e3c8
Jason <jmcservv@gmail.com>
2018-06-28 08:27:03 +0200

    Add files via upload

---
+++ fws_example.ino
@@ -0,0 +1,24 @@
+float sensorValue;
+// the setup routine runs once when you press reset:
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  // read the input on analog pin 0:
+  sensorValue = analogRead(A5);
+  float average = 0;
+  for (int i = 0; i < 10; i++) {
+    average = average + sensorValue / 10;
+    delay(1);
+  }
+float voltage = average * (5.0 / 1023.0);
+
+  Serial.print("dia: ");
+
+  Serial.println(voltage);
+
+delay(10);
+}
